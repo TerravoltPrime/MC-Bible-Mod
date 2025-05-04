ab19fedc7bb2f0990d6b3f2d27ce6b43ef8b4a42
Terravolt <terravolt.mc@gmail.com>
2025-05-04 16:50:54 -0400

    Hello World

--- src/main/java/com/Terravolt/fruitofspirit/FruitOfSpirit.java
+++ src/main/java/com/Terravolt/fruitofspirit/FruitOfSpirit.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.Terravolt.fruitofspirit;
 
 import org.slf4j.Logger;
 
@@ -35,8 +35,8 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(FruitOfSpirit.MODID)
+public class FruitOfSpirit
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -69,7 +69,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public FruitOfSpirit(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -82,7 +82,7 @@ public class ExampleMod
         CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Note that this is necessary if and only if we want *this* class (FruitOfSpirit) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
